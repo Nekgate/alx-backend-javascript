9d64ac380c8e6d352f8b6890b6ad38ce907bed4b
Nekgate <Nekgate@gmail.com>
2024-07-01 21:47:40 +0100

    Task 2,  Filter

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,3 +1,8 @@
+/**
+ * Retrieves a list of students.
+ * @author Timothy Goodluck <https://github.com/Nekgate>
+ * @returns {{id: Number, firstName: String, location: String}[]}
+ */
 export default function getListStudentIds(students) {
   if (Array.isArray(students)) {
     return students.map((items) => items.id);
