c03b1649fc46c1cb06b5b00903d1e7ccd8fe30a5
Nekgate <Nekgate@gmail.com>
2024-08-20 11:53:43 +0000

    Executing basic javascript with Node JS

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,12 +1,12 @@
 /**
- * Function to print a message in console
- * @param {string} message - Message to print
- */
+* Function to print a message in the console
+* @param {string} message - Message to print
+*/
 
 function displayMessage(str) {
-    console.log(str);  // Prints the message to the console
+  console.log(str); // Print the message to the console
 }
 
 // Export function to make it available to other modules
-module.exports = displayMessage
+module.exports = displayMessage;
 
