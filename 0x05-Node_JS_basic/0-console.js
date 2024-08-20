cdfe0c8276218d4210aa7f5cb0cf1f3cb30ec4cf
Nekgate <Nekgate@gmail.com>
2024-08-20 11:40:42 +0000

    Executing basic javascript with Node JS

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,12 @@
+/**
+ * Function to print a message in console
+ * @param {string} message - Message to print
+ */
+
+function displayMessage(str) {
+    console.log(str);  // Prints the message to the console
+}
+
+// Export function to make it available to other modules
+module.exports = displayMessage
+
---
+++ 0x05-Node_JS_basic/README.md
@@ -0,0 +1,8 @@
+# ALX Back-end REPO.
+0x05. NodeJS Basics
+
+Back-end
+JavaScript
+ES6
+NodeJS
+ExpressJS
